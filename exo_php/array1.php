042560e076a9c21f789bc8929234987a54b31c11
Safia54 <sbihmedn@gmail.com>
2017-05-05 10:51:15 +0200

    formulaire

---
+++ exo_php/array1.php
@@ -0,0 +1,21 @@
+<DOCTYPE!>
+<html>
+  <head>
+    <title> test array </title>
+  </head>
+  <body>
+
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+$moi = array( 'Baba', 'Mama', 'Hmed', 'Sfeya', 'Toutmène' );
+print_r($famille);
+#echo $famille;
+
+?>
+
+  </body>
+</html>
